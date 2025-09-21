ec3b80477ba94d4ec72d7800f15e51377f14b93d
Imran1476 <imranlf2028@gmail.com>
2025-09-22 01:02:57 +0600

    third

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './country.css'
 
-const Country = ({country}) => {
-    console.log(country.flags.flags.png)
+const Country = ({country , handleVisitedCountries}) => {
+ 
+    const [visited ,setVisited]= useState(false)
+    //    console.log( handleVisitedCountries)
+    // console.log(country.area.area)
+    const handleVisit=()=>{
+        // setVisited(true)
+        // console.log('clicked')
+        // first
+        // if(visited){
+        //     setVisited(false)
+        // } else {
+        //     setVisited(true)
+        // }
+
+        // second
+
+        // setVisited(visited ? false :true)
+        // third
+        setVisited(!visited);
+        handleVisitedCountries(country);      
+    }
     return (
-        <div>
+        <div className={`country  ${visited && "country-visited"}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags}/>
             <h4>Name:{country.name.common}</h4>
             <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : " Small Country"}</p>
+            <button onClick={handleVisit}>{visited ? "visited" :"Not Visited"}</button>
         </div>
     );
 };
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,19 +1,30 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/Country';
+import './countries.css';
 
 const Countries = ({countriesPromise}) => {
+    const [visitedCountries , setVisitedCountries]= useState([]);
+    const handleVisitedCountries=(country)=>{
+        console.log("visited countires clicked",country)
+    }
     const countriesData=use(countriesPromise);
     const countries=countriesData.countries;
-console.log(countries )
+// console.log(countries )
     return (
-        <div>
-           <h2>In the Country:{countries.length}</h2> 
+         <div>
+            <h2>In the Country:{countries.length}</h2> 
+            <h4>Visited Countries:</h4>
+        <div className='countries'>
+          
            {
-            countries.map(country=> <Country country={country}>
-
+            countries.map(country=> <Country 
+            country={country}
+             handleVisitedCountries={handleVisitedCountries}>
+ 
             </Country> )
            }
         </div>
+         </div>
     );
 };
 
